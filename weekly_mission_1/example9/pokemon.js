ad679ff9c62b0c7612f17cc5822d3e23e942a286
Gustavo Lira <gu.lirar23@gmail.com>
2022-04-14 17:42:08 -0500

    add example9 files to weekly_mission_1

---
+++ weekly_mission_1/example9/pokemon.js
@@ -0,0 +1,14 @@
+
+
+export default class Pokemon{
+    constructor (name){
+        this.name = name
+    }
+
+    sayHello(){
+        console.log('Mi pokemon ' + this.name +' te saluda!!!')
+    }
+    sayMessage(){
+        console.log('Mi pokemon ' + this.name + ' dice: Heey!')
+    }
+}
