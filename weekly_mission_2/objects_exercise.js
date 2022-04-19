3f1dcf3e780da3a004a07b93f45daf7a0674a60d
Gustavo Lira <gu.lirar23@gmail.com>
2022-04-19 11:04:28 -0500

    add: object exercise and the first 3 scripts

---
+++ weekly_mission_2/objects_exercise.js
@@ -0,0 +1,16 @@
+/* 
+
+Objetos en JavaScript
+
+*/
+
+//Example1: create a empty object
+let myObject = {}
+console.log(myObject)
+
+//Example2: create a object with properties
+let myObject2_user = {
+    name: "John",
+    last_name: "Doe"
+}
+console.log(myObject2_user)
